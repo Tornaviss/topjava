759b8a32e4b3346cfd375ac1aa4e39cba7aa3177
Tornaviss <tornaviss@gmail.com>
2019-06-07 12:37:19 +0300

    Added static Map variable for storing calories consumed by day with public getter.

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,8 +1,14 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class UserMeal {
+
+    private static final SortedMap<LocalDate, Integer> caloriesSumByDay = new TreeMap<>();
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -13,6 +19,12 @@ public class UserMeal {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        if (!caloriesSumByDay.containsKey(dateTime.toLocalDate())) caloriesSumByDay.put(dateTime.toLocalDate(), calories);
+        caloriesSumByDay.put(dateTime.toLocalDate(), caloriesSumByDay.get(dateTime.toLocalDate()) + calories);
+    }
+
+    public static SortedMap<LocalDate, Integer> getCaloriesSumByDay() {
+        return caloriesSumByDay;
     }
 
     public LocalDateTime getDateTime() {
