3d5ca321004f1d5f985bc27c85987256f67b0b31
Tornaviss <tornaviss@gmail.com>
2019-06-16 08:38:00 +0300

    Removed all hardcoded code from MealsUtil class

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -13,18 +13,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MealsUtil {
-    public static void main(String[] args) {
-        List<Meal> meals = Arrays.asList(
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
-        );
-        List<MealTo> mealsWithExcess = getFilteredWithExcess(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsWithExcess.forEach(System.out::println);
-    }
 
     public static List<MealTo> getFilteredWithExcess(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
@@ -37,17 +25,7 @@ public class MealsUtil {
                 .filter(meal -> TimeUtil.isBetween(meal.getTime(), startTime, endTime))
                 .map(meal ->
                         new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                                caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
+                                caloriesSumByDate.get(meal.getDate()) > caloriesPerDay, meal.getId()))
                 .collect(Collectors.toList());
-    }
-    public static List<MealTo> getHardcodedMealToList() {
-        return Arrays.asList(
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500, false),
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000, false),
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500, false),
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000, true),
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500, true),
-                new MealTo(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510, true)
-        );
     }
 }
