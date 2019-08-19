03fd65b46fe358ac106943e647435710673453a0
Tornaviss <tornaviss@gmail.com>
2019-08-19 12:20:39 +0300

    9 04 binding

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import ru.javawebinar.topjava.model.AbstractBaseEntity;
 
-import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static java.util.Objects.requireNonNull;
 
 public class SecurityUtil {
 
