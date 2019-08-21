a2df8793b56338204a98d3f8e1e1b54f2c83e76e
Tornaviss <tornaviss@gmail.com>
2019-08-21 10:28:37 +0300

    HW9 + optional

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,25 +1,37 @@
+const mealAjaxUrl = "ajax/profile/meals/";
+
 function updateFilteredTable() {
     $.ajax({
         type: "GET",
-        url: "ajax/profile/meals/filter",
+        url: mealAjaxUrl + "filter",
         data: $("#filter").serialize()
     }).done(updateTableByData);
 }
 
 function clearFilter() {
     $("#filter")[0].reset();
-    $.get("ajax/profile/meals/", updateTableByData);
+    $.get(mealAjaxUrl, updateTableByData);
 }
 
 $(function () {
     makeEditable({
-        ajaxUrl: "ajax/profile/meals/",
+        ajaxUrl: mealAjaxUrl,
         datatableApi: $("#datatable").DataTable({
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
+                    "data": "dateTime",
+                    "render": function (date, type, row) {
+                        if (type === "display") {
+                            return date.substring(0, 10) + " " + date.substring(11, 16);
+                        }
+                        return date;
+                    }
                 },
                 {
                     "data": "description"
@@ -29,11 +41,13 @@ $(function () {
                 },
                 {
                     "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "render": renderEditBtn
                 },
                 {
                     "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -41,8 +55,60 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function(row, date, dateIndex) {
+                $(row).attr("data-mealExcess", date.excess)
+            }
         }),
-        updateTable: updateFilteredTable
+        updateTable: function() {
+            $.get(mealAjaxUrl, updateFilteredTable);
+        }
     });
+    enableDatetimepicker();
 });
+
+function enableDatetimepicker() {
+    $("#dateTime").datetimepicker({
+        format: "Y-m-d H:i"
+    });
+    const startDate = $("#startDate");
+    const endDate = $("#endDate");
+    startDate.datetimepicker({
+        format:'Y-m-d',
+        onShow:function(ct){
+            this.setOptions({
+                maxDate: endDate.val() ? endDate.val() : false
+            })
+        },
+        timepicker:false
+    });
+    endDate.datetimepicker({
+        format:'Y-m-d',
+        onShow:function(ct){
+            this.setOptions({
+                minDate: startDate.val() ? startDate.val() : false
+            })
+        },
+        timepicker:false
+    });
+    const startTime = $("#startTime");
+    const endTime = $("#endTime");
+    startTime.datetimepicker({
+        format: "H:i",
+        onShow: function(ct) {
+            this.setOptions({
+                maxTime: endTime.val() ? endTime.val() : false
+            })
+        },
+        datepicker: false
+    });
+    endTime.datetimepicker({
+        format: "H:i",
+        onShow: function(ct) {
+            this.setOptions({
+                minTime: startTime.val() ? startTime.val() : false
+            })
+        },
+        datepicker: false
+    })
+}
